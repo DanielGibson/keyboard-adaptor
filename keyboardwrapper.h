3c0bac26c9b4a4e570a1821d32e876cd989173aa
Daniel Gibson <metalcaedes@gmail.com>
2021-10-03 05:21:50 +0200

    Make it compatible with Arduino IDE
    I used Sloeber for the main work, but for just building and uploading
    the project, the Arduino IDE is easier to set up and to use.
    (And even though it's subpar as a text editor, it should be sufficient
     for small things like adding a new keyboard to InputKeyboard.cpp)

--- keyboardwrapper.h
+++ keyboardwrapper.h
@@ -16,8 +16,10 @@
 
 //add your function definitions for the project keyboardwrapper here
 
-
-
+#ifndef CDC_DISABLED
+// uncomment to log some debug info to the Serial console
+//#define KBDWRAP_ENABLE_DEBUG
+#endif
 
 //Do not add code below this line
 #endif /* _keyboardwrapper_H_ */
