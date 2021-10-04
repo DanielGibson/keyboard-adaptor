7939f94d7ee585a5b3aecae986a14ea05e1f5e47
Daniel Gibson <metalcaedes@gmail.com>
2021-10-04 06:14:02 +0200

    Small improvements to HexPrinter
    - Use the unsinged int type the corresponds to INT_T instead of always
      using uint64_t - should be faster, esp. on Arduino
    - Remove comment about endianess (it doesn't matter)
    - Make nibble a char, so it matches what we want to feed into p.print()
    - Return numnibbles instead of adding up the return vals of p.print()

--- DGHelpers.hpp
+++ DGHelpers.hpp
@@ -19,8 +19,18 @@
 
 #include "Arduino.h"
 
+// the stuff in this namespace is just implementation details
+// below it are the more generally useful functions
 namespace dg_impl
 {
+
+// UnsignedForSize<sizeof(MyIntegerType)>::type is the unsigned integer type
+// for (with the same size as) MyIntegerType
+template<int size> struct UnsignedForSize;
+template<> struct UnsignedForSize<1> { typedef uint8_t type; };
+template<> struct UnsignedForSize<2> { typedef uint16_t type; };
+template<> struct UnsignedForSize<4> { typedef uint32_t type; };
+template<> struct UnsignedForSize<8> { typedef uint64_t type; };
 
 template<typename INT_T>
 struct HexPrinter final : public Printable
@@ -32,28 +42,26 @@ struct HexPrinter final : public Printable
 
 	size_t printTo(Print& p) const override
 	{
-		size_t ret = 0;
-
 		// Number of nibbles
-		size_t numnibbles = sizeof(INT_T) * 2;
+		int numnibbles = sizeof(INT_T) * 2;
 
-		// Assume that we'll get 64 bits at max.
-		uint64_t converted = (uint64_t)val;
+		// always print value as unsigned (like printf's %x)
+		typedef typename UnsignedForSize<sizeof(INT_T)>::type UINT_T;
+		UINT_T converted = (UINT_T)val;
 
-		// For little endian only!
 		for (int n = numnibbles; n != 0; n--) {
 			// Extract nibble...
-			uint32_t nibble = (converted >> (4 * (n - 1))) & 0x0F;
+			char nibble = (converted >> (4 * (n - 1))) & 0x0F;
 
 			// ...and print it.
 			if (nibble <= 9) {
-				ret += p.print((char)nibble + '0');
+				p.print(nibble + '0');
 			} else {
-				ret += p.print((char)nibble - 10 + 'A');
+				p.print(nibble - 10 + 'A');
 			}
 		}
 
-		return ret;
+		return numnibbles;
 	}
 };
 
@@ -71,8 +79,12 @@ inline void _PrintHelper(const T1& arg1, const Ts&... args)
 
 } //namespace dg_impl
 
+
+
 // returns a class derived from Printable that will print x (which should be an integer type) as hex
 // Serial.print(AsHex(42)); will print 42 in hex (without 0x prefix!)
+// And in contrast to Serial.print(var, 16), it will always print all bytes of an int,
+// even leading 0-bytes, so (int16)1 will be printed as "0001" instead of just "01"
 // more useful for PrintAll("fourtytwo = ", AsHex(42), " var = ", AsHex(var));
 template<typename INT_T>
 inline dg_impl::HexPrinter<INT_T> AsHex(INT_T x)
